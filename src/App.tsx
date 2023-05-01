be43c6b5179447f47417a68dcecec3d2444a0df7
Cory House <housecor@gmail.com>
2023-05-01 14:20:06 -0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { foods } from "./types/food";
 
 export function App() {
-  const appetizers = foods.filter((food) => food.tags.includes("Appetizer"));
+  const appetizers = foods; //.filter((food) => food.tags.includes("Appetizer"));
   return (
     <>
       <h1>Menu</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h2>Appetizers</h2>
-      {appetizers.map((food) => (
-        <p key={food.id}>
-          {food.name} - ${food.price}
-        </p>
-      ))}
+      <section className="flex flex-wrap">
+        {appetizers.map((food) => (
+          <div
+            className="border w-96 border-cyan-600 p-4 max-w-md m-4 rounded shadow-md bg-slate-200"
+            key={food.id}
+          >
+            <div className="flex">
+              <div>
+                <h3 className="text-lg font-bold">{food.name}</h3>
+                <p>{food.description}</p>
+                <p className="mb-4 mt-4">
+                  <span className="font-bold">Tags:</span>{" "}
+                  {food.tags.join(", ")}
+                </p>
+                <p className="font-bold">${food.price}</p>
+              </div>
+              <img src={food.image} alt={food.name} className="h-32 ml-4" />
+            </div>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
